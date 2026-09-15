e362f725343aadba7786bd50ccf54bdbcdc906b4
pentel679-sys <pentel679@gmail.com>
2026-09-15 13:26:26 +0900

    feat:fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -10,50 +11,61 @@ import Favorites from "./pages/Favorites";
 
 import "./App.css";
 
-// type Page = "landing" | "login" | "signup";
-
-// export default function App() {
-//   const [page, setPage] = useState<Page>("landing");
-
-//   // 로그인 화면
-//   if (page === "login") {
-//     return (
-//       <Login
-//         onNavigateSignup={() => setPage("signup")}
-//       />
-//     );
-//   }
-
-//   // 회원가입 화면
-//   if (page === "signup") {
-//     return (
-//       <Signup
-//         onNavigateLogin={() => setPage("login")}
-//       />
-//     );
-//   }
-
-//   return (
-//     <Landing
-//       onNavigateLogin={() => setPage("login")}
-//       onNavigateSignup={() => setPage("signup")}
-//     />
-//   );
-// }
-
+type Page =
+  | "landing"
+  | "login"
+  | "signup"
+  | "exercise"
+  | "dashboard"
+  | "places"
+  | "favorites";
 
 export default function App() {
-  switch (window.location.pathname.replace(/\/+$/, "") || "/") {
-    case "/exercise":
-      return <ExerciseInfo />;
+  const [page, setPage] = useState<Page>("landing");
 
-    case "/places":
-      return <NearbyPlaces />;
-
-    case "/favorites":
-      return <Favorites />;
-
-    default:
-      return <Dashboard />;
+  // 로그인 화면
+  if (page === "login") {
+    return (
+      <Login
+        onLoginSuccess={() => setPage("exercise")}
+        onNavigateSignup={() => setPage("signup")}
+      />
+    );
   }
+
+  // 회원가입 화면
+  if (page === "signup") {
+    return (
+      <Signup
+        onNavigateLogin={() => setPage("login")}
+      />
+    );
+  }
+
+  // 운동 정보
+  if (page === "exercise") {
+    return <ExerciseInfo />;
+  }
+
+  // 대시보드
+  if (page === "dashboard") {
+    return <Dashboard />;
+  }
+
+  // 주변 운동 장소
+  if (page === "places") {
+    return <NearbyPlaces />;
+  }
+
+  // 즐겨찾기
+  if (page === "favorites") {
+    return <Favorites />;
+  }
+
+  // 처음 화면
+  return (
+    <Landing
+      onNavigateLogin={() => setPage("login")}
+    />
+  );
 }
